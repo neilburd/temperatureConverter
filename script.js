4b6793e3d231191b5b8067c216a5354c221d65d0
“neilburd” <neilburd@gmail.com>
2016-11-15 22:03:37 -0500

    Celcius to Fahrenheit is working

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+//create the conversion function
+let tempF = function(){
+
+    inputTemp = document.getElementById('temp').value;
+
+    console.log(inputTemp);
+
+    let finalTemp = Math.round((inputTemp - 32)/1.8);
+
+    //Alert
+    alert (inputTemp + "Fahrenheit" + "is " + finalTemp + " in Celsius.");
+}
+
+//run the function on click
+document.getElementById('cToF').addEventListener('click', function(){tempF()});
+
+//console.log(finalTemp);
